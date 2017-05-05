4b32e405e1d0120060f859ef37aea7d88e3c1d26
Majid Hajiloo <majid.hajiloo@gmail.com>
2017-05-05 18:37:20 +0430

    add Josephus problem solution

---
+++ problems/josephus.py
@@ -0,0 +1,18 @@
+def josephus(sequence: list, skip: int):
+    result = []
+
+    skip = skip - 1
+    idx = 0
+
+    while len(sequence) > 0:
+        idx = (idx + skip) % len(sequence)
+        result.append(sequence.pop(idx))
+
+    return result
+
+
+t_list = list(input())
+
+result = josephus(t_list, 3)
+print(''.join(result[:-1]))
+print('survivor: {}'.format(result[-1]))
